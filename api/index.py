fba5d43bf453d473810e73f81e777f15e0e20034
Balaji Seetharaman <balaji_ceg@outlook.com>
2024-10-26 22:44:53 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
 @app.route('/about')
 def about():
     return 'About'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
